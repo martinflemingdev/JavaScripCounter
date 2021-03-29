5fb22667758fd5eca1bbe03882dfaf77f956c825
Martin Fleming <martinflemingdev@gmail.com>
2021-03-29 19:53:49 -0400

    Working Counter!

---
+++ app.js
@@ -0,0 +1,30 @@
+// set initial count
+let count = 0;
+console.log('HELO')
+// select value and buttons
+
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach( (btn) => {
+    btn.addEventListener('click', (event) => {
+        const styles = event.currentTarget.classList;
+        if (styles.contains("decrease")) {
+            count--;
+        } else if (styles.contains("increase")) {
+            count++;
+        } else {
+            count = 0;
+        }
+        if (count<0) {
+            value.style.color = 'red';
+        } 
+        if (count>0) {
+            value.style.color = 'green'; 
+        }
+        if (count===0) {
+            value.style.color = 'black';
+        }
+        value.textContent = count;
+    });
+});
